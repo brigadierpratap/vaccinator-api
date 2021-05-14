9ee1f009fd8a5bb15808e5e6f011a25025187346
Pawan Singh <pmarsingh@gmail.com>
2021-05-14 11:35:47 +0530

    test 2

--- functions/checkStatus.js
+++ functions/checkStatus.js
@@ -76,6 +76,7 @@ const getStatus = async u => {
         accept: "application/json, text/plain, */*",
         "Content-Type": "application/json",
         Authorization: "None",
+        connection: "keep-alive",
         "accept-encoding": "gzip, deflate, br",
         "accept-language": "en-GB,en-US;q=0.9,en;q=0.8,hi;q=0.7",
         "if-none-match": 'W/"c11-wux5YlARbvOJeXedTVUeMM02jJs"',
@@ -85,7 +86,7 @@ const getStatus = async u => {
           '" Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"',
         "sec-ch-ua-mobile": "?0",
         "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
+        "sec-fetch-mode": "no-cors",
         "sec-fetch-site": "cross-site",
         "user-agent":
           "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",
